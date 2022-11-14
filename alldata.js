5407a32401f6fdb5226b6aaad129446ef2ffd612
Daphne Benzaquen <98908807+dbenzaquen89@users.noreply.github.com>
2022-11-14 15:25:38 -0600

    Delete alldata.js

--- alldata.js
+++
@@ -1,9 +0,0 @@
-function AllData(){
-    const ctx = React.useContext(userContext);
-    return(
-        <>
-        <h1>AllData Component</h1>
-        {JSON.stringify(ctx)}
-        </>
-    )
-}
